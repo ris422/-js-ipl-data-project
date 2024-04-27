8dc683d2731be2e6de81985da7d130c505c0f4e8
ris422 <81076276+ris422@users.noreply.github.com>
2024-04-27 18:44:35 +0530

    Update matches-per-year.js

--- src/server/matches-per-year.js
+++ src/server/matches-per-year.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 const csv = require('csv-parser');
-const { promises } = require('dns');
 
 function readCSV(filePath){
     return new Promise((resolve, reject) => {
